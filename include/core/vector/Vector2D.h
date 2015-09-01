e936afb83ad531b51512c15313730da40bad51b7
Babouchot <bastien.maureille@hotmail.fr>
2015-09-01 13:54:44 +0200

    base build system and Vector2D

---
+++ include/core/vector/Vector2D.h
@@ -0,0 +1,26 @@
+#ifndef __VECTOR2D_H__
+#define __VECTOR2D_H__
+
+namespace core {
+
+namespace vector {
+
+
+class Vector2D {
+ private:
+  int x;
+  int y;
+
+ public:
+  Vector2D(int x = 0, int y = 0);
+};
+
+
+
+
+}
+
+}
+
+
+#endif
---
+++ src/core/vector/Vector2D.cpp
@@ -0,0 +1,12 @@
+#include <core/vector/Vector2D.h>
+
+namespace core {
+
+namespace vector {
+
+Vector2D::Vector2D(int x, int y) : x(x), y(y) {}
+
+
+}
+
+}
